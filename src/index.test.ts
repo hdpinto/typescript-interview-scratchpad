b93373e9521afcea21c438fc2f8965890910bf16
Herlander Diogo Pinto <hdpinto@outlook.com>
2025-03-28 09:48:06 +0100

    Feat: Upgrade packages and use vitest (#1)
    * chore: updating packages, adding engine, using pnpm
    
    * chore: update tsconfig
    
    * feat: using vitest
    
    * feat: project vscode settings
    
    * adding eslint rule for eol-last

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,9 @@
-import "jest";
-
 import { doSomething, multiply } from ".";
+import { vi } from "vitest";
 
 describe("doSomething", () => {
   it("should output to the console", () => {
-    const spy = jest.spyOn(console, "log");
+    const spy = vi.spyOn(console, "log");
     doSomething("testing");
     expect(spy).toHaveBeenCalledWith("I am currently testing");
   });
